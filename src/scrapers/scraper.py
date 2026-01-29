6792e49f5ce104466fd05315ec45f3f39ccafb69
Renwarren <warren.chendjou@gmail.com>
2026-01-28 19:39:06 -0500

    Add support for ultiple languages

--- src/scrapers/scraper.py
+++ src/scrapers/scraper.py
@@ -2,39 +2,49 @@ import asyncio
 import aiohttp
 from typing import List
 
-from .config import BASE_URL, PAGE_DELAY, LETTER_DELAY
+from .config import PAGE_DELAY, LETTER_DELAY, QUERY_TEMPLATE
 from .api import fetch_html
+from .languages import LanguageConfig
 from .models import DictionaryEntry
 from .parser import extract_entries, extract_total_pages
 
 
 async def scrape_letter(
     session: aiohttp.ClientSession,
+    language: LanguageConfig,
     letter: str,
 ) -> List[DictionaryEntry]:
-    entries: List[DictionaryEntry] = []
+    results: List[DictionaryEntry] = []
 
-    first_url = BASE_URL.format(letter=letter, page=1)
+    first_url = (
+        f"{language.base_url}"
+        f"{QUERY_TEMPLATE.format(letter=letter, page=1)}"
+    )
+
     soup = await fetch_html(session, first_url)
-
-    entries.extend(extract_entries(soup))
+    results.extend(extract_entries(soup))
     total_pages = extract_total_pages(soup)
 
     for page in range(2, total_pages + 1):
         await asyncio.sleep(PAGE_DELAY)
-        url = BASE_URL.format(letter=letter, page=page)
+        url = (
+            f"{language.base_url}"
+            f"{QUERY_TEMPLATE.format(letter=letter, page=page)}"
+        )
         soup = await fetch_html(session, url)
-        entries.extend(extract_entries(soup))
+        results.extend(extract_entries(soup))
 
-    return entries
+    return results
 
 
-async def scrape_all_letters() -> List[DictionaryEntry]:
+async def scrape_language(language: LanguageConfig) -> List[DictionaryEntry]:
     all_entries: List[DictionaryEntry] = []
 
     async with aiohttp.ClientSession() as session:
         for letter in "abcdefghijklmnopqrstuvwxyz":
-            all_entries.extend(await scrape_letter(session, letter))
+            all_entries.extend(
+                await scrape_letter(session, language, letter)
+            )
             await asyncio.sleep(LETTER_DELAY)
 
     return all_entries
