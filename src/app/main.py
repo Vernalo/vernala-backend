ce7b960007705e0724ade6c654a5f38dd698824f
Renwarren <warren.chendjou@gmail.com>
2026-01-30 21:42:24 -0500

    Cors middleware

--- src/app/main.py
+++ src/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes.health_router import router as health_router
 from app.routes.translate_router import router as translate_router
 from app.routes.languages_router import router as languages_router
@@ -15,6 +16,16 @@ app = FastAPI(
 app.include_router(health_router)
 app.include_router(translate_router)
 app.include_router(languages_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "*"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 if __name__ == "__main__":
